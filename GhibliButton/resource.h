029b2887d0a46157621e07a9b6a08d093b4e9651
kenjinote <2605401+kenjinote@users.noreply.github.com>
2022-08-13 23:24:04 +0900

    プロジェクト ファイルを追加します。

---
+++ GhibliButton/resource.h
@@ -0,0 +1,38 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// GhibliButton.rc で使用
+//
+#define IDC_MYICON                      2
+#define IDD_GHIBLIBUTTON_DIALOG         102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_GHIBLIBUTTON                107
+#define IDI_SMALL                       108
+#define IDC_GHIBLIBUTTON                109
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     140
+#define IDB_BITMAP2                     141
+#define IDB_BITMAP3                     142
+#define IDB_BITMAP4                     143
+#define IDB_BITMAP5                     144
+#define IDB_BITMAP6                     145
+#define IDB_BITMAP7                     146
+#define IDB_BITMAP8                     147
+#define IDB_BITMAP9                     148
+#define IDB_BITMAP10                     149
+#define IDB_BITMAP11                     150
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        151
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
