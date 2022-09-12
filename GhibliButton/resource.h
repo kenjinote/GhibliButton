dfe79857f249623e891d7f238aa5f15fc2ebf777
kenjinote <2605401+kenjinote@users.noreply.github.com>
2022-09-13 01:05:10 +0900

    画像追加

--- GhibliButton/resource.h
+++ GhibliButton/resource.h
@@ -31,6 +31,7 @@
 #define IDB_BITMAP17                     156
 #define IDB_BITMAP18                     157
 #define IDB_BITMAP19                     158
+#define IDB_BITMAP20                     159
 
 #define IDC_STATIC                      -1
 
@@ -39,7 +40,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        159
+#define _APS_NEXT_RESOURCE_VALUE        160
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
